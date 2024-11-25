226a089a72eba24a4f1d89e478112df2d97605b2
wojciechmakowski <makowski.w@icloud.com>
2024-11-25 21:55:55 +0100

    Initial Commit

---
+++ MatchUp/MatchUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchUpApp.swift
+//  MatchUp
+//
+//  Created by Wojciech Makowski on 25/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MatchUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
